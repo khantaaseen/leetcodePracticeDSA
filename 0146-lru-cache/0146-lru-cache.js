e023554e226c5d66de3c078b6c1e4621e6e26f7e
Taaseen Khan <91350132+khantaaseen@users.noreply.github.com>
2026-09-26 01:18:56 -0400

    Time: 18 ms (99.36%), Space: 69.2 MB (75.32%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.js
+++ 0146-lru-cache/0146-lru-cache.js
@@ -1,3 +1,13 @@
+class Node{
+    constructor(key, value){
+        this.key = key;
+        this.value = value;
+
+        this.prev = null;
+        this.next = null;
+    }
+}
+
 /**
  * @param {number} capacity
  */
@@ -5,7 +15,30 @@ var LRUCache = function(capacity) {
 
     this.cacheMap = new Map();
     this.size = capacity;
+
+    this.head = new Node(0,0);
+    this.tail = new Node(0,0);
+
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+    
 };
+
+LRUCache.prototype.insert = function(node){
+
+    let mru = this.tail.prev;
+
+    mru.next = node;
+    node.prev = mru;
+
+    node.next = this.tail;
+    this.tail.prev = node;
+}
+
+LRUCache.prototype.remove = function(node){
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+}
 
 /** 
  * @param {number} key
@@ -16,11 +49,12 @@ LRUCache.prototype.get = function(key) {
         return -1;
     }
 
-    let val = this.cacheMap.get(key);
+    const node = this.cacheMap.get(key);
 
-    this.cacheMap.delete(key);
-    this.cacheMap.set(key, val);
-    return this.cacheMap.get(key);
+    this.remove(node);
+    this.insert(node);
+
+    return node.value;
 };
 
 /** 
@@ -30,20 +64,29 @@ LRUCache.prototype.get = function(key) {
  */
 LRUCache.prototype.put = function(key, value) {
 
-    if(!this.cacheMap.has(key)){
-        if(this.cacheMap.size + 1 > this.size){
-            var lru = this.cacheMap.keys().next().value;
-            this.cacheMap.delete(lru);
+    if(this.cacheMap.has(key)){
+        const node = this.cacheMap.get(key);
 
-            this.cacheMap.set(key, value);
-        }
-        else{
-            this.cacheMap.set(key, value);
-        }
-    }else{
-        this.cacheMap.delete(key);
-        this.cacheMap.set(key, value);
+        node.value = value;
+
+        this.remove(node);
+        this.insert(node);
+
+        return;
     }
+
+    const node = new Node(key, value);
+    this.insert(node);
+    this.cacheMap.set(key, node);
+
+    if(this.cacheMap.size > this.size){
+        const lru = this.head.next;
+
+        this.remove(lru);
+        this.cacheMap.delete(lru.key);
+    }
+
+    return;
 };
 
 /** 
